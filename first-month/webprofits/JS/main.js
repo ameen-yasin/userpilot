84f555bc4bf79ea91379922e3cce6426f6498032
ameen-yasin <ameeen_1998@hotmail.com>
2021-02-27 21:46:42 +0200

    web-profit

--- first-month/webprofits/JS/main.js
+++ first-month/webprofits/JS/main.js
@@ -2,56 +2,56 @@ const spanElement = document.querySelector('.company-name');
 // 
 // console.log(spanElement.innerHTML= "hello");
 
-      
-var opacity=0; 
-var intervalID=0; 
-window.onload=fadeout; 
+
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeout;
 // setTimeout(1000);
 // setTimeout(function(){ alert("Hello"); }, 500);
-setTimeout(fadeIn,500);
+setTimeout(fadeIn, 500);
 
-function fadeout(){ 
-       setInterval(hide, 200); 
+function fadeout() {
+    setInterval(hide, 200);
 }
-function fadeIn(){ 
-    setInterval(show, 200); 
+function fadeIn() {
+    setInterval(show, 200);
 }
 
 
-function hide(){
+function hide() {
     //   var body=document.getElementById("body"); 
-      opacity = Number(window.getComputedStyle(spanElement).getPropertyValue("opacity")) 
-        if(opacity>0){ 
-               opacity=opacity-0.5; 
-               spanElement.style.opacity=opacity 
-        } 
-        else{ 
-            clearInterval(intervalID);  
-        } 
+    opacity = Number(window.getComputedStyle(spanElement).getPropertyValue("opacity"))
+    if (opacity > 0) {
+        opacity = opacity - 0.5;
+        spanElement.style.opacity = opacity
     }
-
-function show(){
-        //   var body=document.getElementById("body"); 
-          opacity = Number(window.getComputedStyle(spanElement).getPropertyValue("opacity")) 
-            if(opacity<=0){ 
-                   opacity=opacity+0.5; 
-                   spanElement.style.opacity=opacity 
-            } 
-            else{ 
-                clearInterval(intervalID);  
-            } 
+    else {
+        clearInterval(intervalID);
     }
+}
 
-    
+function show() {
+    //   var body=document.getElementById("body"); 
+    opacity = Number(window.getComputedStyle(spanElement).getPropertyValue("opacity"))
+    if (opacity <= 0) {
+        opacity = opacity + 0.5;
+        spanElement.style.opacity = opacity
+    }
+    else {
+        clearInterval(intervalID);
+    }
+}
 
 
-    const toggleItem=document.querySelector('.toggle');
-const showCaseItem=document.querySelector('.showcase');
-
-toggleItem.addEventListener('click',showMenu);
 
 
-function showMenu(e){
+const toggleItem = document.querySelector('.toggle');
+const showCaseItem = document.querySelector('.showcase');
+
+toggleItem.addEventListener('click', showMenu);
+
+
+function showMenu() {
     toggleItem.classList.toggle('active');
     showCaseItem.classList.toggle('active');
 }
